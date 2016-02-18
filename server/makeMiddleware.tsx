d2a5183ee60823b6dc1853ab7d9273beab4e99ec
Howard Yeh <howard@metacircus.com>
2016-02-18 15:36:23 +0800

    fix error handling problem

--- server/makeMiddleware.tsx
+++ server/makeMiddleware.tsx
@@ -6,32 +6,96 @@ const { Router, RouterContext, match, createMemoryHistory } = require('react-rou
 
 import { DirectServiceFactory } from "./DirectServiceFactory";
 import { createStore } from "../store";
-import { ServerServices, HYDRATION_DATA_NAME, REACT_ROOT_ID, SERVICE_CURSOR_PATH } from "../index";
 import { ServiceContext } from "../react";
+
+import {
+  ServerServices,
+  HYDRATION_DATA_NAME,
+  REACT_ROOT_ID,
+  SERVICE_CURSOR_PATH,
+  Routes,
+} from "../index";
 
 interface MiddlewareConfig {
 }
 
-export function makeMiddleware (
+export function makeMiddleware(
   routes: any,
   services: ServerServices,
   StaticHTMLWrapper: React.ComponentClass<any> | React.StatelessComponent<any>,
   config?: MiddlewareConfig
-  ) {
-  // const { routes, services, scripts, StaticHTMLWrapper } = config;
+) {
 
-  async function matchRoute(req, res, next) {
+  async function handleRoute(req, res, routerProps): Promise<void> {
+    const store = createStore();
+
+    const factory = new DirectServiceFactory(services, store.select(SERVICE_CURSOR_PATH), {
+      req,
+    });
+    // const service;
+
+    const App = () => {
+      return (
+        <ServiceContext serviceFactory={factory}>
+          <RouterContext {...routerProps}>
+            <Router routes={routes} history={history}/>
+          </RouterContext>
+        </ServiceContext>
+      );
+    };
+
+    // Q: Does construction happen without rendering?
+    // A: doesn't look like it's happening.
+    // const app = <App/>;
+
+    // res.end(JSON.stringify({redirectLocation, renderProps}, null, 2));
+
+    /*
+      LOLZ Two rendering passes.
+    */
+    // First rendering pass
+    // TODO make Layout a no-op if data isn't loaded yet
+    let output: string = ReactDOM.renderToString(<App/>);
+
+
+    if (factory.hasPendingLoadRequests) {
+      // Second rendering pass, with loaded data
+      console.log("throw away first pass", output);
+      await factory.waitLoadRequests();
+      output = ReactDOM.renderToString(<App/>);
+    }
+
+    const hydrationData = store.get();
+
+    // cast to any to supress typescript error...
+    const StaticHTMLWrapper_: any = StaticHTMLWrapper;
+
+    const viewWithStaticWrapper = (
+      <StaticHTMLWrapper_ {...routerProps}>
+        <div id={REACT_ROOT_ID} dangerouslySetInnerHTML={{ __html: output }}/>
+        {hydrationData && <InjectHydrationData data={hydrationData}/>}
+      </StaticHTMLWrapper_>
+    );
+
+    res.write("<!doctype html>\n");
+    res.write(ReactDOM.renderToStaticMarkup(viewWithStaticWrapper));
+    res.end();
+
+    return;
+  }
+
+  function matchRoute(req, res, next) {
     const history = createMemoryHistory();
     const location = createLocation(req.url);
 
     // Ignore requests that looks like JSON.
     // In particular, we want `Accept: */*` to not match if we are curling the API.
-    if(req.path.match(/\.json$/) || req.headers.accept.match("json")) {
+    if (req.path.match(/\.json$/) || req.headers.accept.match("json")) {
       next();
       return;
     }
 
-    match({ routes, location }, async (error, redirectLocation, renderProps) => {
+    match({ routes, location }, (error, redirectLocation, renderProps) => {
       if (error) {
         next(error);
         return;
@@ -42,69 +106,10 @@ export function makeMiddleware (
         return;
       }
 
-      // FIXME remove after experiment
-      // let loadPromises: Promise<any>[] = [];
-      // global.loadPromises = loadPromises;
-
-      const store = createStore();
-
-      const factory = new DirectServiceFactory(services, store.select(SERVICE_CURSOR_PATH), {
-        req,
-      });
-      // const service;
-
-      const App = () => {
-        return (
-          <ServiceContext serviceFactory={factory}>
-            <RouterContext {...renderProps}>
-              <Router routes={routes} history={history}/>
-            </RouterContext>
-          </ServiceContext>
-        );
-      };
-
-      // Q: Does construction happen without rendering?
-      // A: doesn't look like it's happening.
-      // const app = <App/>;
-
-      // res.end(JSON.stringify({redirectLocation, renderProps}, null, 2));
-
-      /*
-        LOLZ Two rendering passes.
-      */
-      // First rendering pass
-      // TODO make Layout a no-op if data isn't loaded yet
-      let output: string = ReactDOM.renderToString(<App/>);
-
-
-      if(factory.hasPendingLoadRequests) {
-        // Second rendering pass, with loaded data
-        console.log("throw away first pass", output);
-        await factory.waitLoadRequests();
-        output = ReactDOM.renderToString(<App/>);
-      }
-
-      const hydrationData = store.get();
-
-      // cast to any to supress typescript error...
-      const StaticHTMLWrapper_: any = StaticHTMLWrapper;
-
-      const viewWithStaticWrapper = (
-        <StaticHTMLWrapper_ {...renderProps}>
-          <div id={REACT_ROOT_ID} dangerouslySetInnerHTML={{__html: output}}/>
-          {hydrationData && <InjectHydrationData data={hydrationData}/>}
-        </StaticHTMLWrapper_>
-      );
-
-	    res.write("<!doctype html>\n");
-      res.write(ReactDOM.renderToStaticMarkup(viewWithStaticWrapper));
-      res.end();
+      handleRoute(req, res, renderProps).catch(next);
     });
   }
-
-	return (req, res, next) => {
-    matchRoute(req, res, next).catch(next);
-  };
+  return matchRoute;
 }
 
 function InjectHydrationData(props) {
