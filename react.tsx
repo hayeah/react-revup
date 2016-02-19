f60bfaea089e79fd5ab2cf5165c57fb722f88c3f
Howard Yeh <howard@metacircus.com>
2016-02-19 15:25:40 +0800

    fuss

--- react.tsx
+++ react.tsx
@@ -1,13 +1,2 @@
-import React = require("react");
-
-import {
-  Service,
-  Store,
-  ServerServices,
-  ServiceManager,
-} from "./index";
-
 export {ServiceContext} from "./react/ServiceContext";
-
 export {ServiceRoot} from "./react/ServiceRoot";
-
